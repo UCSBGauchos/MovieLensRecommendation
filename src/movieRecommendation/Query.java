7667a5a0ffe57e52ae7be0b3e4dfbfd604e6ba9a
UCSBGauchos <byang@cs.ucsb.edu>
2014-02-28 17:55:22 -0800

    Query

--- src/movieRecommendation/Query.java
+++ src/movieRecommendation/Query.java
@@ -11,6 +11,8 @@ import org.apache.hadoop.mapred.JobConf;
 
 public class Query {
 	public static void main(String [] args) throws IOException {
+		HashMap<String, Float> localHash = new HashMap<String, Float>();
+		HashMap<String, Integer> localUserRating = new HashMap<String, Integer>();
 		JobConf job = new JobConf();
 //		Path queryPath = new Path("query");
 //		FileSystem hadoopFS = queryPath.getFileSystem(job);
@@ -22,9 +24,12 @@ public class Query {
 //		String line;
 		
 		//input the id of the movie
-//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//		System.out.print("Enter movie id of the movie you want its predicted rate: ");
-//		String movieID = br.readLine();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print("Enter movie id of the movie you want its predicted rate: ");
+		String movieID = br.readLine();
+		
+		System.out.print("Enter movie userid: ");
+		String userID = br.readLine();
 
 		//neighbour: read from KNNData
 		//user: read From UserData
@@ -42,7 +47,38 @@ public class Query {
 		}
 		FSDataInputStream UserData = hadoopFS.open(UserResult);
 		
-		
-		
+		String neighbourtLine;
+		while ((neighbourtLine = KNNData.readLine()) != null){
+			//System.out.println(line.toString());
+			StringTokenizer token = new StringTokenizer(neighbourtLine.toString(), " |\t,");
+			String mid = token.nextToken();
+			if(mid.equals(movieID)){
+				while(token.hasMoreTokens()){
+					String neighbourmid = token.nextToken();
+					float wij = Float.parseFloat(token.nextToken());
+					localHash.put(neighbourmid, wij);
+				}
+			}
+		}
+		//System.out.println(localHash);
+		String userLine;
+		while ((userLine = UserData.readLine()) != null){
+			StringTokenizer token = new StringTokenizer(userLine.toString(), " |\t");
+			while(token.hasMoreTokens()){
+				String uid = token.nextToken();
+				String mid = token.nextToken();
+				Integer rating = Integer.parseInt(token.nextToken());
+				String time = token.nextToken();
+				if(uid.equals(userID)){
+					localUserRating.put(mid, rating);
+				}
+			}
+		}
+		if(localUserRating.keySet().contains(movieID)){
+			System.out.println("User "+userID+" has rated "+localUserRating.get(movieID)+" to the movie "+movieID+" no need to predict");
+		}else{
+			System.out.println("Need to predict the rating which "+userID+" gives to the movie "+movieID);
+			
+		}
 	}
 }
