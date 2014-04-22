b22c9a2137cde8e28debd1123c08e5bbacdcf758
UCSBGauchos <byang@cs.ucsb.edu>
2014-04-22 00:15:21 -0700

    some local sync

--- src/movieRecommendation/SlopeOneTraining.java
+++ src/movieRecommendation/SlopeOneTraining.java
@@ -13,6 +13,18 @@ public class SlopeOneTraining {
 	
 	public static void main(String [] args) throws IOException {
 		JobConf job = new JobConf();
+		//user1 movie1 98 movie2 97
+		//user2 movie1 100
+		//user3 movie 100
+		//only choose those users which rate both i and j
+		HashMap<String, HashMap<String, Integer>> chosenUserInfo = new HashMap<String, HashMap<String, Integer>>();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//System.out.print("Enter movie userid: ");
+		//String userID = br.readLine();
+		System.out.print("Enter movie id of the movie you want its predicted rate: ");
+		String movieID = br.readLine();	
+		String tmp = "20";//tmp chose i as 10;
+		
 		
 		Path SlopeOnePreprocessResult = new Path("SlopeOnePreprocess/part-00000");
 		FileSystem hadoopFS = SlopeOnePreprocessResult.getFileSystem(job);
@@ -21,11 +33,41 @@ public class SlopeOneTraining {
 		}
 		FSDataInputStream SlopeOnePreprocessData = hadoopFS.open(SlopeOnePreprocessResult);
 		
-		String readLine;
+		String itemLine;
 		
-		while((readLine = SlopeOnePreprocessData.readLine()) != null){
-			System.out.println(readLine);
+		while((itemLine = SlopeOnePreprocessData.readLine()) != null){
+			StringTokenizer token = new StringTokenizer(itemLine.toString(), " |\t,");
+			StringTokenizer token2 = new StringTokenizer(itemLine.toString(), " |\t,");
+			String uid = token.nextToken();
+			boolean containsI = false;
+			boolean containsJ = false;
+			while(token.hasMoreTokens()){
+				String mid = token.nextToken();
+				Integer rating = Integer.parseInt(token.nextToken());
+				if(mid.equals(movieID)){
+					containsJ = true;
+				}
+				if(mid.equals(tmp)){
+					containsI = true;
+				}
+			}
+			//if this line contains both movie i and j, then it will be written to the list
+			if(containsI == true && containsJ == true){
+				String uid2 = token2.nextToken();
+				while(token2.hasMoreTokens()){
+					String mid2 = token2.nextToken();
+					Integer rating2 = Integer.parseInt(token2.nextToken());
+					HashMap<String, Integer> value = new HashMap<String, Integer>();
+					value.put(mid2, rating2);
+					if(!chosenUserInfo.containsKey(uid2)){
+						chosenUserInfo.put(uid2, value);
+					}else{
+						chosenUserInfo.get(uid2).put(mid2, rating2);
+					}
+				}
+			}
 		}
+		System.out.println(chosenUserInfo.keySet());
 	}
 	
 }
